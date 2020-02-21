ca2191f065547dd0f4eeb9aeaca86b10847a9465
DanilToLinus <danil-lomakin-02@mail.ru>
2020-02-21 22:01:04 +0500

    Rendering tabs.

--- practice/text_editor/kilo.c
+++ practice/text_editor/kilo.c
@@ -19,6 +19,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define ABUF_INIT {NULL, 0}
 #define KILO_VERSION "0.0.1"
+#define KILO_TAB_STOP 8
 
 enum editorKey {
     ARROW_LEFT = 1000,
@@ -34,10 +35,12 @@ enum editorKey {
 
 /*** data ***/
 
-// a data type for storing a row of text in our editor.
+// a data type (buffer) for storing a row of text in our editor.
 typedef struct erow {
-    int size;
-    char* chars;
+    int size; // size of a line.
+    int rsize;
+    char* chars; // the line.
+    char* render;
 } erow;
 
 // The editor settings.
@@ -245,6 +248,30 @@ int getWindowSize(int *rows, int * cols) {
 
 /*** row operations ***/
 
+void editorUpdateRow(erow* row) {
+    // count how many tabs keeps a line (also called row).
+    int tabs = 0;
+    for (int i = 0; i < row->size; i++)
+        if (row->chars[i] == '\t') tabs++;
+
+    // allocate the nessessary amount of memory.
+    free(row->render);
+    row->render = malloc(row->size + tabs * (KILO_TAB_STOP - 1) + 1);
+
+    // copy the line from chars to render with spaces instead of tabs.
+    int idx = 0;
+    for (int i = 0; i < row->size; i++) {
+        if (row->chars[i] == '\t')
+            do
+                row->render[idx++] = ' ';
+            while (idx % KILO_TAB_STOP != 0);
+        else
+            row->render[idx++] = row->chars[i];
+    }
+    row->render[idx] = '\0';
+    row->rsize = idx;
+}
+
 void editorAppendRow(char* s, size_t len) {
     // reallocate the block of memory for the new row.
     E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
@@ -256,6 +283,11 @@ void editorAppendRow(char* s, size_t len) {
     E.row[at].chars = malloc(len + 1);
     memcpy(E.row[at].chars, s, len);
     E.row[at].chars[len] = '\0';
+
+    E.row[at].rsize = 0;
+    E.row[at].render = NULL;
+    editorUpdateRow(&E.row[at]);
+
     E.numrows++;
 }
 
@@ -453,10 +485,10 @@ void editorDrawRows(struct abuf *ab) {
             }
         }
         else {
-            int len = E.row[filerow].size - E.coloff;
+            int len = E.row[filerow].rsize - E.coloff;
             if (len < 0) len = 0;
             if (len > E.screencols) len = E.screencols;
-            abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+            abAppend(ab, &E.row[filerow].render[E.coloff], len);
         }
 
         abAppend(ab, "\x1b[K", 3);
